9af6e558beafc84ac2ae118f964cb58f3f80825d
Henry <henryng161@gmail.com>
2024-06-23 16:20:44 +0700

    Fix linting error by delete unused variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useSyncExternalStore } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import TaskList from "./components/TaskList";
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import reactDom, { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-const root = reactDom.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 
 root.render(<App />);
